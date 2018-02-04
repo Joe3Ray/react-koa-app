9e3d0ae5a9d03cb52816b7a4bc141f515fbc72a0
JoeRay61 <joeray199261@gmail.com>
2018-02-04 21:30:35 +0800

    feat: 完成异步 action 示例

--- app/containers/hello/action.js
+++ app/containers/hello/action.js
@@ -1,8 +1,16 @@
 import * as types from './constant'
+import axios from 'axios'
 
 export function fetchUserInfo () {
-  return {
-    type: types.FETCH_INFO,
-    url: '/common/app_info'
+  return (dispatch) => {
+    axios.get('/common/app_info')
+      .then(res => {
+        if (res.statusText === 'OK' && res.data && res.data.code === 0) {
+          dispatch({
+            type: types.FETCH_INFO,
+            result: res.data.msg
+          })
+        }
+      })
   }
 }
--- server/controllers/common/info.js
+++ server/controllers/common/info.js
@@ -1,7 +1,8 @@
 module.exports = router => {
   router.get('/common/app_info', async ctx => {
     ctx.body = {
-      code: 0
+      code: 0,
+      msg: 'this is react-koa-app'
     }
   })
 }
